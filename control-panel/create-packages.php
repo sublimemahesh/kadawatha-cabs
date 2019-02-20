d7d1d148bbb7e1a458cdbc0433f85c3d144c06a5
MTSUdayanga <sachithgalle@gmail.com>
2019-02-20 11:32:47 +0530

    #38 Create Package

--- control-panel/create-packages.php
+++ control-panel/create-packages.php
@@ -114,6 +114,20 @@ include_once(dirname(__FILE__) . '/auth.php');
                                         </div>
                                     </div>
 
+                                    <div class="row clearfix">
+                                        <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                            <label for="name">Description </label>
+                                        </div>
+                                        <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                                            <div class="form-group form-float">
+                                                <label class="form-label"></label>
+                                                <div class="form-line">
+                                                    <textarea id="description" name="description" class="form-control" rows="5"></textarea> 
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
                                     <div class="col-lg-offset-2 col-md-offset-2 col-sm-offset-4 col-xs-offset-5">
                                         <input type="submit" name="create" class="btn btn-primary m-t-15 waves-effect" value="create"/>
                                     </div>
